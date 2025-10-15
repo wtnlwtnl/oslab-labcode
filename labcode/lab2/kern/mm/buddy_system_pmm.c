e7e9ae6e5e8560c5ef6814c8957808ebcb25ef03
Cai Zixuan <3575746113@qq.com>
2025-10-15 23:33:30 +0800

    lab2 1

---
+++ labcode/lab2/kern/mm/buddy_system_pmm.c
@@ -0,0 +1,283 @@
+#include <pmm.h>
+#include <list.h>
+#include <string.h>
+#include <buddy_system_pmm.h>
+#include <stdio.h>
+
+static free_area_t free_area[15];
+static struct Page *page_base = NULL;
+static uint32_t nr_free = 0;
+
+#define free_list(x) (free_area[x].free_list)
+#define nr_free(x) (free_area[x].nr_free)
+
+static void
+buddy_system_init(void) {
+    for(int i = 0; i < 15; ++i) {
+        list_init(&free_list(i));
+        nr_free(i) = 0;
+    }
+}
+
+static void
+buddy_system_init_memmap(struct Page *base, size_t n) {
+    assert(n > 0);
+    struct Page *p = base;
+    page_base = base;
+    for (; p != base + n; p ++) {
+        assert(PageReserved(p));
+        p->flags = p->property = 0;
+        set_page_ref(p, 0);
+    }
+    nr_free += n;
+    for(int i = 0; i < 15; ++i) if((n >> i) & 1) {
+        struct Page *q = base + (n -= 1 << i);
+        q->property = 1 << i;
+        SetPageProperty(q);
+        list_add(&free_list(i), &(q->page_link));
+        ++ nr_free(i);
+    }
+}
+
+static struct Page *
+buddy_system_alloc_pages(size_t n) {
+    assert(n > 0);
+    // cprintf("Alloc : %d\n", n);
+    if (n > nr_free) {
+        return NULL;
+    }
+    struct Page *page = NULL;
+    uint8_t log = 0;
+    for(int i = 0; i < 15; ++i) {
+        if(n <= (1 << i) && nr_free(i)) {
+            page = le2page(list_next(&free_list(log = i)), page_link);
+            break;
+        }
+    }
+    // cprintf("%d  %d\n", log, nr_free(log));
+    if (page != NULL) {
+        list_del(&(page->page_link));
+        --nr_free(log);
+        while(log && n <= (1 << (log - 1))) {
+            ++nr_free(--log);
+            // cprintf("%d\n", log);
+            struct Page *p = page + (1 << log);
+            p->property = (1 << log);
+            p->flags = 0;
+            set_page_ref(p, 0);
+            SetPageProperty(p);
+            list_add_before(&free_list(log), &(p->page_link));
+        }
+        nr_free -= 1 << log;
+        page->property = 1 << log;
+        ClearPageProperty(page);
+    }
+    return page;
+}
+
+static void
+buddy_system_free_pages(struct Page *base, size_t n) {
+    assert(n > 0);
+    size_t page_id = base - page_base;
+    base->property = n;
+    SetPageProperty(base);
+    nr_free += n;
+
+    for(int i = 0; i < 15; ++i) if((n >> i) & 1){
+        size_t buddy_page_id = page_id ^ (1 << i);
+        struct Page *buddy_page = page_base + buddy_page_id;
+        if(PageProperty(buddy_page) && buddy_page->property == base->property) {
+            if(buddy_page_id < page_id)
+                page_id = buddy_page_id;
+            base = page_base + page_id;
+            base->property = 2 << i;
+            list_del(&(buddy_page->page_link));
+            --nr_free(i);
+            ClearPageProperty(base + (1 << i));
+            n <<= 1;
+        } else {
+            list_add_before(&free_list(i), &(base->page_link));
+            ++nr_free(i);
+            break;
+        }
+    }
+}
+
+static size_t
+buddy_system_nr_free_pages(void) {
+    return nr_free;
+}
+
+static void
+basic_check(void) {
+    struct Page *p[1000];
+    size_t total = buddy_system_nr_free_pages();
+    for(int i = 0; i < 100; ++i) {
+        assert((p[i] = alloc_pages(i % 32 + 1)) != NULL);
+        assert(page_ref(p[i]) == 0);
+        assert(page2pa(p[i]) < npage * PGSIZE);
+        assert((total -= p[i]->property) == buddy_system_nr_free_pages());
+    }
+    for(int i = 100; i < 1000; ++i) {
+        assert((p[i] = alloc_page()) != NULL);
+        assert(page_ref(p[i]) == 0);
+        assert(page2pa(p[i]) < npage * PGSIZE);
+        assert((total -= p[i]->property) == buddy_system_nr_free_pages());
+    }
+    for(int i = 0; i < 1000; ++i) {
+        for(int j = i + 1; j < 1000; ++j) {
+            // cprintf("%d  %d  %d  %d\n", i, j, p[i] - page_base, p[j] - page_base);
+            assert(p[i] != p[j]);
+        }
+    }
+    for(int i = 0; i < 100; i += 2) {
+        total += p[i]->property;
+        free_pages(p[i], p[i]->property);
+        assert(total == buddy_system_nr_free_pages());
+    }
+    for(int i = 100; i < 200; i += 2) {
+        total += p[i]->property;
+        free_pages(p[i], p[i]->property);
+        assert(total == buddy_system_nr_free_pages());
+    }
+    for(int i = 0; i < 100; i += 2) {
+        assert((p[i] = alloc_pages(i % 32 + 1)) != NULL);
+        assert(page_ref(p[i]) == 0);
+        // cprintf("%d  %u  %u\n", p[i] - page_base, page2pa(p[i]), npage * PGSIZE);
+        assert(page2pa(p[i]) < npage * PGSIZE);
+        assert((total -= p[i]->property) == buddy_system_nr_free_pages());
+    }
+    for(int i = 100; i < 200; i += 2) {
+        assert((p[i] = alloc_page()) != NULL);
+        assert(page_ref(p[i]) == 0);
+        // cprintf("%d  %u  %u\n", p[i] - page_base, page2pa(p[i]), npage * PGSIZE);
+        assert(page2pa(p[i]) < npage * PGSIZE);
+        assert((total -= p[i]->property) == buddy_system_nr_free_pages());
+    }
+    for(int i = 200; i < 1000; ++i) {
+        total += p[i]->property;
+        free_pages(p[i], p[i]->property);
+        assert(total == buddy_system_nr_free_pages());
+    }
+    for(int i = 200; i < 1000; ++i) {
+        assert((p[i] = alloc_page()) != NULL);
+        assert(page_ref(p[i]) == 0);
+        // cprintf("%d  %u  %u\n", p[i] - page_base, page2pa(p[i]), npage * PGSIZE);
+        assert(page2pa(p[i]) < npage * PGSIZE);
+        assert((total -= p[i]->property) == buddy_system_nr_free_pages());
+    }
+    for(int i = 0; i < 1000; ++i) {
+        for(int j = i + 1; j < 1000; ++j) {
+            // cprintf("%d  %d  %d  %d\n", i, j, p[i] - page_base, p[j] - page_base);
+            assert(p[i] != p[j]);
+        }
+    }
+    // list_entry_t free_list_store = free_list;
+    // list_init(&free_list);
+    // assert(list_empty(&free_list));
+
+    // unsigned int nr_free_store = nr_free;
+    // nr_free = 0;
+
+    // assert(alloc_page() == NULL);
+
+    // free_page(p0);
+    // free_page(p1);
+    // free_page(p2);
+    // assert(nr_free == 3);
+
+    // assert((p0 = alloc_page()) != NULL);
+    // assert((p1 = alloc_page()) != NULL);
+    // assert((p2 = alloc_page()) != NULL);
+
+    // assert(alloc_page() == NULL);
+
+    // free_page(p0);
+    // assert(!list_empty(&free_list));
+
+    // struct Page *p;
+    // assert((p = alloc_page()) == p0);
+    // assert(alloc_page() == NULL);
+
+    // assert(nr_free == 0);
+    // free_list = free_list_store;
+    // nr_free = nr_free_store;
+    for(int i = 0; i < 1000; ++i)
+        free_page(p[i]);
+}
+
+// LAB2: below code is used to check the first fit allocation algorithm (your EXERCISE 1) 
+// NOTICE: You SHOULD NOT CHANGE basic_check, default_check functions!
+static void
+default_check(void) {
+    // int count = 0, total = 0;
+    // list_entry_t *le = &free_list;
+    // while ((le = list_next(le)) != &free_list) {
+    //     struct Page *p = le2page(le, page_link);
+    //     assert(PageProperty(p));
+    //     count ++, total += p->property;
+    // }
+    // assert(total == nr_free_pages());
+
+    basic_check();
+
+    // struct Page *p0 = alloc_pages(5), *p1, *p2;
+    // assert(p0 != NULL);
+    // assert(!PageProperty(p0));
+
+    // list_entry_t free_list_store = free_list;
+    // list_init(&free_list);
+    // assert(list_empty(&free_list));
+    // assert(alloc_page() == NULL);
+
+    // unsigned int nr_free_store = nr_free;
+    // nr_free = 0;
+
+    // free_pages(p0 + 2, 3);
+    // assert(alloc_pages(4) == NULL);
+    // assert(PageProperty(p0 + 2) && p0[2].property == 3);
+    // assert((p1 = alloc_pages(3)) != NULL);
+    // assert(alloc_page() == NULL);
+    // assert(p0 + 2 == p1);
+
+    // p2 = p0 + 1;
+    // free_page(p0);
+    // free_pages(p1, 3);
+    // assert(PageProperty(p0) && p0->property == 1);
+    // assert(PageProperty(p1) && p1->property == 3);
+
+    // assert((p0 = alloc_page()) == p2 - 1);
+    // free_page(p0);
+    // assert((p0 = alloc_pages(2)) == p2 + 1);
+
+    // free_pages(p0, 2);
+    // free_page(p2);
+
+    // assert((p0 = alloc_pages(5)) != NULL);
+    // assert(alloc_page() == NULL);
+
+    // assert(nr_free == 0);
+    // nr_free = nr_free_store;
+
+    // free_list = free_list_store;
+    // free_pages(p0, 5);
+
+    // le = &free_list;
+    // while ((le = list_next(le)) != &free_list) {
+    //     struct Page *p = le2page(le, page_link);
+    //     count --, total -= p->property;
+    // }
+    // assert(count == 0);
+    // assert(total == 0);
+}
+
+const struct pmm_manager buddy_system_pmm_manager = {
+    .name = "buddy_system_manager",
+    .init = buddy_system_init,
+    .init_memmap = buddy_system_init_memmap,
+    .alloc_pages = buddy_system_alloc_pages,
+    .free_pages = buddy_system_free_pages,
+    .nr_free_pages = buddy_system_nr_free_pages,
+    .check = default_check,
+};
+
