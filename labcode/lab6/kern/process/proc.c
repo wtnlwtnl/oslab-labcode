e7e123e1ae0299bafd6e114c6708cfc6d5d09c86
gaoyudong830-bit <gaoyudong830@google.com>
2026-01-03 21:04:42 +0800

    update lab6 report

--- labcode/lab6/kern/process/proc.c
+++ labcode/lab6/kern/process/proc.c
@@ -142,7 +142,7 @@ alloc_proc(void)
         proc->time_slice = 0;
         proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
         proc->lab6_stride = 0;
-        proc->lab6_priority = 0;
+        proc->lab6_priority = 1;
     }
     return proc;
 }
